e15df7083012a7f6898436cc521377d707ca3fe2
hema9866 <169077510+hema9866@users.noreply.github.com>
2025-01-09 18:15:01 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Use the full file path (make sure to escape backslashes in the path)
+file_path = r'C:\Users\Hema1089\Desktop\intenship\supply_chain_data.csv'
+
+# Read the CSV file
+data = pd.read_csv(file_path)
